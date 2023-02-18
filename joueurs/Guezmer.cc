59fe5791733d97f3d08b7f16f8eb0c19fb20a4f5
Ahmad <ahmadhouhou1@outlook.com>
2023-02-18 21:53:05 +0100

    Ajout nbPartiePere
    - modification du Qubc
    - ajout "." pour etatPartie +"." +std::to_string(...)
    - modification de la fonction coupEstConnu en ajoutant "."

--- joueurs/Guezmer.cc
+++ joueurs/Guezmer.cc
@@ -41,7 +41,7 @@ void Guezmer::majEtatPartie(couple coup,int tour) {
 
 bool Guezmer::coupEstConnu(couple coup) const {
     for (const auto& elem : movesStruct) {
-        if (elem.id == etatPartie + std::to_string(coup.first) + std::to_string(coup.second)) {
+        if (elem.id == etatPartie +"."+ std::to_string(coup.first) + std::to_string(coup.second)) {
             return true;
         }
     }
@@ -52,6 +52,7 @@ void Guezmer::recherche_coup(Jeu j, couple &coup)
 {
     bool toutLesCoupsSontConnus = true;
     int taille = j.coups_possibles().size();
+    std::cout<<"etat : "<<etatPartie;
     //afficher les coups possibles
    /* 
     std::cout << "coups possibles" << std::endl;
@@ -71,16 +72,30 @@ void Guezmer::recherche_coup(Jeu j, couple &coup)
             return;
         }
     }
+
     // si tous les coups sont connus on utilise le qubc pour connaitre le meilleur coup
     if (toutLesCoupsSontConnus) {
         float max = 0;
+        int nbPartiePere = 0;
         for (int i = 0; i < taille; i++) {
             for (const auto& elem : movesStruct) {
-                if (elem.id == etatPartie + std::to_string(j.coups_possibles()[i].first) + std::to_string(j.coups_possibles()[i].second)) {
+
+                // on prend en compte que l'etatPartie est l'idPere 
+                //pour rceuperer le nombre de partie du pere
+
+                if(elem.id == etatPartie){
+                    nbPartiePere = elem.nbPartie;
+                }
+
+                if (elem.id == etatPartie +"." +std::to_string(j.coups_possibles()[i].first) + std::to_string(j.coups_possibles()[i].second)) {
                     // si vous voulez regarder les qubc de chaque coup
                     //std::cout << "qubc : " << qubc(elem.score, elem.nbPartie, taille) << " pour le coup : " << j.coups_possibles()[i].first << j.coups_possibles()[i].second << std::endl;
-                    if (qubc(elem.score, elem.nbPartie, taille) > max) {
-                        max = qubc(elem.score, elem.nbPartie, taille);
+
+                    std::cout<<"indice qubc : "<<elem.id <<" "<<elem.score<<" "<<nbPartiePere<<" "<<elem.nbPartie<<std::endl;
+                    std::cout<<"qubc : "<<qubc(elem.score,nbPartiePere,elem.nbPartie)<<std::endl;
+
+                    if (qubc(elem.score,nbPartiePere,elem.nbPartie) > max) {
+                        max = qubc(elem.score,nbPartiePere,elem.nbPartie);
                         // maj a jour du meilleur coup
                         coup.first=j.coups_possibles()[i].first;
                         coup.second=j.coups_possibles()[i].second;
