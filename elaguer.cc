40845eaad3e9688c8e4be54c4ec43da56023fc38
Henrico49 <alexandre.salgueirohenriques@gmail.com>
2023-03-07 10:21:14 +0100

    Elaguer fonctionnelle
    fonctionne mais pas dingue

--- elaguer.cc
+++ elaguer.cc
@@ -1,15 +1,12 @@
-#include "Lecteur.hh"
-#include <vector>
-#include <unordered_map>
 #include <iostream>
+#include <sstream>
 #include "ArbitreENT.hh"
 #include "jeu.hh"
+#include "lecteur.hh"
+#include <cmath> // Ajout nécessaire
+#include <unistd.h>
 
-struct coupStruct{
-  std::string id;
-  int score;
-  int nbPartie;
-};
+
 
 struct coupStructPre{
   std::string id;
@@ -29,6 +26,7 @@ std::string extractPrefix(const std::string& id) {
 
 int elaguer()
 {
+std::vector<std::string> Bestmoves;
 std::vector<std::string> moves;
 Lecteur l1("../coups.csv");
 l1.lire(moves);
@@ -46,16 +44,20 @@ for (const auto& elem : moves) {
     ss >> score;
     ss.ignore();
     ss >> nbPartie;
-    movesMap[id] = {id, score, nbPartie, id_prefix};
+    if(nbPartie>5)
+        movesMap[id] = {id, score, nbPartie, id_prefix};
+    else
+        Bestmoves.push_back(id + "," + std::to_string(score) + "," + std::to_string(nbPartie));
+
 }
+std::cout<<"le fichier est chargé"<<std::endl;
 
-
-std::cout<<"test 1 "<<std::endl;
 std::vector<std::unordered_map<std::string, coupStructPre>> VectPre;
 for (const auto& [id, elem] : movesMap) {
     // Extraire le préfixe de l'id
     std::string id_prefix = elem.pre;
     bool prefix_found = false;
+    
 
     // Chercher si un élément avec ce préfixe existe déjà dans VectPre
     for (auto& pre_map : VectPre) {
@@ -66,23 +68,36 @@ for (const auto& [id, elem] : movesMap) {
         prefix_found = true;
         break;
     }
-}
+    }
 
     // Si aucun élément avec ce préfixe n'a été trouvé, créer un nouvel unordered_map
     if (!prefix_found) {
         std::unordered_map<std::string, coupStructPre> new_map;
-        new_map[id_prefix] = elem;
+        new_map[id] = elem;
         VectPre.push_back(new_map);
     }
 }
 
-//print vectpre
-for (const auto& p : VectPre) {
-    for (const auto& [id, elem] : p) {
-        std::cout << id << " " << elem.score << " " << elem.nbPartie << " " << elem.pre << std::endl;
+// Itérer sur les éléments de VectPre
+for (const auto& pre_map : VectPre) {
+    // Initialiser les variables pour le score maximum et l'élément correspondant
+    int max_score = -10000;
+    coupStructPre max_elem = {"", -1, -1, ""};
+    
+    // Parcourir chaque élément de l'unordered_map et trouver l'élément avec le score maximum
+    for (const auto& [id, elem] : pre_map) {
+        if (elem.score*100/elem.nbPartie > max_score) {
+            max_score = elem.score*100/elem.nbPartie;
+            max_elem = elem;
+        }
     }
+    
+    // Ajouter l'élément avec le score maximum à la liste des meilleurs coups
+    Bestmoves.push_back(max_elem.id + "," + std::to_string(max_elem.score) + "," + std::to_string(max_elem.nbPartie));
 }
 
 
-}
+    Lecteur l2("../coupsElaguer.csv");
+    l2.write(Bestmoves);
 
+}
